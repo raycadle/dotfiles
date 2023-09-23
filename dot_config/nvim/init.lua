5da7361e41429964f51c9401d9eb0ef4fc51813f
N3WK1D <devel.kepada@aleeas.com>
2023-09-23 02:42:34 -0600

    Added keymaps loading call.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,4 +6,5 @@ Neovim Configuration
 -- Import Modules
 --------------------
 require("config.options") -- Neovim Options
+require("config.keymaps") -- Neovim Keymaps
 require("config.lazy") -- Lazy.nvim Options
