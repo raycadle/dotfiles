17440437cd9629a17fa345a4e51335fb6be0ffb3
raycadle <devel.kepada@aleeas.com>
2024-06-30 21:42:50 -0600

    manually added dev branch

---
+++ README.md
@@ -0,0 +1,33 @@
+# Dotfiles
+A very minimal collection of dotfiles managed by chezmoi.
+
+## Dependencies
+- neovim
+- tmux
+- git
+- tmux
+- fd
+- ripgrep
+- fzf
+- ranger
+- lazygit
+- nerdfonts
+
+## Usage
+After a fresh Linux install, run:
+```shell
+chezmoi init --apply --verbose https://github.com/N3WK1D/dotfiles.git
+```
+
+If chezmoi is not found in the distro's repos, run:
+```shell
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply N3WK1D
+```
+
+For a one-shot install, run:
+```shell
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot N3WK1D
+```
+
+## Reference
+Chezmoi's user manual can be found [here](https://www.chezmoi.io/user-guide/command-overview/).
---
+++ home/private_dot_config/private_nvim/private_lua/private_config/private_core.lua
@@ -0,0 +1,52 @@
+--[[
+-- Neovim Options
+--]]
+
+--------------------
+-- General
+--------------------
+local vg = vim.g -- Global Options
+local vo = vim.opt -- Set Options
+
+vg.loaded_python3_provider = 0
+vg.loaded_ruby_provider = 0
+vg.loaded_node_provider = 0
+vg.loaded_perl_provider = 0
+
+-- Sync clipboard between OS and Neovim
+vg.clipboard = {
+  name = "termux-clipboard",
+  copy = { ["+"] = { "termux-clipboard-set" }, ["*"] = { "termux-clipboard-set" } },
+  paste = { ["+"] = { "termux-clipboard-get" }, ["*"] = { "termux-clipboard-get" } },
+  cache_enabled = true
+}
+
+vo.mouse = "a" -- Enable mouse mode
+vo.ignorecase = true -- Case insensitive searching 
+vo.smartcase = true -- UNLESS /C or capital in search
+vo.splitright = true -- Open new window on the right
+vo.splitbelow = true -- Open new window on the bottom
+vo.termguicolors = true -- Enable terminal colors
+
+--------------------
+-- Neovim UI
+--------------------
+vo.cmdheight = 0 -- Merge statusline and command line
+vo.laststatus = 3 -- Set statusline to always display
+vo.cursorline = true -- Highlight current line in Insert mode
+vo.hlsearch = false -- Highlight on search
+vo.number = true -- Line numbers
+vo.relativenumber = true -- Relative line numbers
+vo.showmatch = true -- Highlight matching parenthesis
+
+--------------------
+-- Tabs & Indent
+--------------------
+vo.autoindent = true -- Enable autoindent
+vo.breakindent = true -- Enable break indent
+vo.expandtab = true -- Use spaces for tab
+vo.shiftwidth = 2 -- Number of spaces for autoindent
+vo.tabstop = 2 -- Number of spaces for tab
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
