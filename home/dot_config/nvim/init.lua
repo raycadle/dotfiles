f5f3e8a0a55a86ec4964381bf8fe84e1c5907482
Ray Cadle <93884664+raycadle@users.noreply.github.com>
2025-08-25 11:32:40 -0600

    Style: numerous config changes
    Added colors to the statusline for different modes, made keybinds more
    consistent, rearranged sections, removed unused functions and keybinds,
    and changed timeoutlen to 1 second, instead of half a second.

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -62,7 +62,7 @@ vim.opt.backup = false -- Don't create backup files
 vim.opt.writebackup = false -- Don't create backup before writing
 vim.opt.swapfile = false -- Don't create swap files
 vim.opt.updatetime = 300 -- Faster completion
-vim.opt.timeoutlen = 500 -- Key timeout duration
+vim.opt.timeoutlen = 1000 -- Key timeout duration
 vim.opt.ttimeoutlen = 0 -- Key code timeout
 vim.opt.autoread = true -- Auto reload files changed outside vim
 vim.opt.autowrite = false -- Don't auto save
@@ -86,173 +86,6 @@ vim.opt.splitright = true -- Vertical splits go right
 
 vim.opt.confirm = true -- Prompt to save before quitting
 
--- Provider Settings
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-
---[[
-============================================================================
-
-    KEYMAPS
-
-============================================================================
---]]
-
-vim.g.mapleader = " " -- Set leader key to space
-vim.g.maplocalleader = " " -- Set local leader key (NEW)
-
-local function map(mode, lhs, rhs, opts)
-    vim.keymap.set(mode, lhs, rhs, opts)
-end
-
--- Quick Config Editing
-map("n", "<leader>rc", "<cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
-
--- Quit
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-
--- Save File
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-
--- New File
-map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
-
--- Quick File Navigation
-map("n", "<leader>e", "<cmd>Explore<CR>", { desc = "Open file explorer" })
-map("n", "<leader>ff", "<cmd>find ", { desc = "Find file" })
-
--- Clear Search With <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
-
--- Better Up/Down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-
--- Better Indenting
-map("v", "<", "<gv", { desc = "Indent left and reselect" })
-map("v", ">", ">gv", { desc = "Indent right and reselect" })
-
--- Terminal Mappings
-map("n", "<C-/>", "<cmd>terminal<cr>", { desc = "Show Terminal" })
-map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
-map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
-map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
-map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
-
--- Windows
-map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
-map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
-map("n", "<leader>wh", "<C-W>s", { desc = "Split window horizontally", remap = true })
-map("n", "<leader>wv", "<C-W>v", { desc = "Split window vertically", remap = true })
-
--- Move to Window Using the <ctrl> hjkl Keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
-
--- Resize Window Using <ctrl> Arrow Keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-
--- Buffers
-map("n", "<leader>bv", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
-map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-
--- Tabs
-map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-map("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
-
--- Center Screen When Jumping
-map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
-map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
-
---[[
-============================================================================
-
-    USEFUL FUNCTIONS
-
-============================================================================
---]]
-
--- Copy Full File-Path
-vim.keymap.set("n", "<leader>pa", function()
-        local path = vim.fn.expand("%:p")
-        vim.fn.setreg("+", path)
-        print("file:", path)
-end)
-
--- Basic autocommands
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
-
--- Highlight yanked text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- Auto-close terminal when process exits
-vim.api.nvim_create_autocmd("TermClose", {
-  group = augroup,
-  callback = function()
-    if vim.v.event.status == 0 then
-      vim.api.nvim_buf_delete(0, {})
-    end
-  end,
-})
-
--- Disable line numbers in terminal
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = augroup,
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = "no"
-  end,
-})
-
--- Auto-resize splits when window is resized
-vim.api.nvim_create_autocmd("VimResized", {
-  group = augroup,
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
-})
-
--- Create directories when saving files
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  callback = function()
-    local dir = vim.fn.expand('<afile>:p:h')
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, 'p')
-    end
-  end,
-})
-
 -- Command-line completion
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
@@ -264,6 +97,12 @@ vim.opt.diffopt:append("linematch:60")
 -- Performance improvements
 vim.opt.redrawtime = 10000
 vim.opt.maxmempattern = 20000
+
+-- Provider Settings
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
 
 --[[
 ============================================================================
@@ -361,15 +200,6 @@ local function CloseFloatingTerminal()
   end
 end
 
--- Key mappings
-map("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "Toggle floating terminal" })
-map("t", "<Esc>", function()
-  if terminal_state.is_open then
-    vim.api.nvim_win_close(terminal_state.win, false)
-    terminal_state.is_open = false
-  end
-end, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
-
 --[[
 ============================================================================
 
@@ -386,15 +216,6 @@ vim.opt.tabline = ''     -- Use default tabline (empty string uses built-in)
 vim.cmd([[
   hi TabLineFill guibg=NONE ctermfg=242 ctermbg=NONE
 ]])
-
--- Alternative navigation (more intuitive)
-map('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
-map('n', '<leader>tx', ':tabclose<CR>', { desc = 'Close tab' })
-
--- Tab moving
-map('n', '<leader>tm', ':tabmove<CR>', { desc = 'Move tab' })
-map('n', '<leader>t>', ':tabmove +1<CR>', { desc = 'Move tab right' })
-map('n', '<leader>t<', ':tabmove -1<CR>', { desc = 'Move tab left' })
 
 -- Function to open file in new tab
 local function open_file_in_tab()
@@ -434,12 +255,6 @@ local function close_tabs_left()
   end
 end
 
--- Enhanced keybindings
-map('n', '<leader>tO', open_file_in_tab, { desc = 'Open file in new tab' })
-map('n', '<leader>td', duplicate_tab, { desc = 'Duplicate current tab' })
-map('n', '<leader>tr', close_tabs_right, { desc = 'Close tabs to the right' })
-map('n', '<leader>tL', close_tabs_left, { desc = 'Close tabs to the left' })
-
 -- Function to close buffer but keep tab if it's the only buffer in tab
 local function smart_close_buffer()
   local buffers_in_tab = #vim.fn.tabpagebuflist()
@@ -450,7 +265,6 @@ local function smart_close_buffer()
     vim.cmd('tabclose')
   end
 end
-map('n', '<leader>bd', smart_close_buffer, { desc = 'Smart close buffer/tab' })
 
 --[[
 ============================================================================
@@ -462,7 +276,7 @@ map('n', '<leader>bd', smart_close_buffer, { desc = 'Smart close buffer/tab' })
 
 -- Mode Indicator
 local function mode_icon()
-  local mode = vim.fn.mode()
+  local current_mode = vim.fn.mode()
   local modes = {
     n = "NORMAL",
     i = "INSERT",
@@ -473,32 +287,61 @@ local function mode_icon()
     s = "SELECT",
     S = "S-LINE",
     ["\19"] = "S-BLOCK",
-    R = "REPLACE",
     r = "REPLACE",
+    R = "REPLACE",
     ["!"] = "SHELL",
     t = "TERMINAL"
   }
-  return modes[mode] or "  " .. mode:upper()
+  return modes[current_mode] or "  " .. current_mode:upper()
 end
 
-_G.mode_icon = mode_icon
-
+-- Define highlight groups for each mode
 vim.cmd([[
-  highlight StatusLineBold gui=bold cterm=bold
+    highlight StatusLineBold gui=bold cterm=bold
+    highlight StatusLineAccent guibg=#89b4fa ctermbg=blue
+    highlight StatusLineInsertAccent guibg=#a6e3a1 ctermbg=green
+    highlight StatusLineVisualAccent guibg=#cba6f7 ctermbg=135
+    highlight StatusLineReplaceAccent guibg=#f38ba8 ctermbg=red
+    highlight StatusLineCmdLineAccent guibg=#fab387 ctermbg=242
+    highlight StatusLineTerminalAccent guibg=#a6e3a1 ctermbg=green
 ]])
+
+local function mode_color()
+  local current_mode = vim.fn.mode()
+  local mode_color = "%#StatusLineAccent#"  -- Default color
+  
+  if current_mode == "n" then
+      mode_color = "%#StatusLineAccent#"
+  elseif current_mode == "i" or current_mode == "ic" then
+      mode_color = "%#StatusLineInsertAccent#"
+  elseif current_mode == "v" or current_mode == "V" or current_mode == "\22" then
+      mode_color = "%#StatusLineVisualAccent#"
+  elseif current_mode == "r" or current_mode == "R" then
+      mode_color = "%#StatusLineReplaceAccent#"
+  elseif current_mode == "c" then
+      mode_color = "%#StatusLineCmdLineAccent#"
+  elseif current_mode == "t" then
+      mode_color = "%#StatusLineTerminalAccent#"
+  end
+  
+  return mode_color
+end
 
 -- Function to change statusline based on window focus
 local function setup_dynamic_statusline()
-  vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+  vim.api.nvim_create_autocmd({"WinEnter", "BufEnter", "ModeChanged"}, {
     callback = function()
     vim.opt_local.statusline = table.concat {
-      " ",
       "%#StatusLineBold#",
-      "%{v:lua.mode_icon()}",
+      mode_color(),
+      " ",
+      mode_icon(),
+      " ",
       "%#StatusLine#",
-      " │ %t %m%r",
+      " %t %m%r",
       "%=",
-      " │ %l/%L:%c ",
+      mode_color(),
+      " %l/%L:%c ",
     }
     end
   })
@@ -506,9 +349,170 @@ local function setup_dynamic_statusline()
 
   vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
     callback = function()
-      vim.opt_local.statusline = " %t %h%m%r %= %{v:lua.file_type()} │ %l:%c │ %P "
+      vim.opt_local.statusline = " %t %m%r %= %l/%L:%c "
     end
   })
 end
 
 setup_dynamic_statusline()
+
+--[[
+============================================================================
+
+    USEFUL FUNCTIONS
+
+============================================================================
+--]]
+
+-- Basic autocommands
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Auto-close terminal when process exits
+vim.api.nvim_create_autocmd("TermClose", {
+  group = augroup,
+  callback = function()
+    if vim.v.event.status == 0 then
+      vim.api.nvim_buf_delete(0, {})
+    end
+  end,
+})
+
+-- Disable line numbers in terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = augroup,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
+  end,
+})
+
+-- Auto-resize splits when window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+-- Create directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand('<afile>:p:h')
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, 'p')
+    end
+  end,
+})
+
+--[[
+============================================================================
+
+    KEYMAPS
+
+============================================================================
+--]]
+
+vim.g.mapleader = " " -- Set leader key to space
+vim.g.maplocalleader = " " -- Set local leader key (NEW)
+
+local function map(mode, lhs, rhs, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Quick Config Editing
+map("n", "<leader>rc", "<cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
+
+-- Quit
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- Save File
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- New File
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
+
+-- Quick File Navigation
+map("n", "<leader>e", "<cmd>Explore<CR>", { desc = "Open file explorer" })
+map("n", "<leader>ff", "<cmd>find ", { desc = "Find file" })
+
+-- Clear Search With <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- Better Up/Down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Better Indenting
+map("v", "<", "<gv", { desc = "Indent left and reselect" })
+map("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Terminal Mappings
+map("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "Open floating terminal" })
+map("t", "<Esc><Esc>", function()
+  if terminal_state.is_open then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+  end
+end, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
+
+-- Windows
+map("n", "<leader>ww", "<C-w>p", { desc = "Other window", remap = true })
+map("n", "<leader>wd", "<C-w>c", { desc = "Delete window", remap = true })
+map("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally", remap = true })
+map("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically", remap = true })
+
+-- Move to Window Using the <ctrl> hjkl Keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Resize Window Using <ctrl> Arrow Keys
+map("n", "<C-UP>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-DOWN>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-LEFT>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-RIGHT>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- Buffers
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map('n', '<leader>bd', smart_close_buffer, { desc = 'Smart close buffer/tab' })
+
+-- Tabs
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map('n', '<leader><tab>>', '<cmd>tabmove +1<CR>', { desc = 'Move Tab Right' })
+map('n', '<leader><tab><', '<cmd>tabmove -1<CR>', { desc = 'Move tab left' })
+map('n', '<leader><tab>O', open_file_in_tab, { desc = 'Open file in new tab' })
+map('n', '<leader><tab>D', duplicate_tab, { desc = 'Duplicate current tab' })
+map('n', '<leader><tab>R', close_tabs_right, { desc = 'Close tabs to the right' })
+map('n', '<leader><tab>L', close_tabs_left, { desc = 'Close tabs to the left' })
+
+-- Center Screen When Jumping
+map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
